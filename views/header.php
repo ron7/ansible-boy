6af799ea71877167c217b293fe592c02e619e04e
Ron <ron@xron.net>
2021-04-29 17:09:49 +0300

    move assets

--- views/header.php
+++ views/header.php
@@ -20,14 +20,14 @@
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link href="<?php echo $webroot; ?>/jquery.dataTables.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="<?php echo $webroot; ?>/assets/jquery.dataTables.min.css" rel="stylesheet" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
     <!-- <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script> -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <script type="text/javascript" src="<?php echo $webroot; ?>/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo $webroot; ?>/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="<?php echo $webroot; ?>/assets/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo $webroot; ?>/assets/jquery.dataTables.min.js"></script>
 
     <style>
 li.nav-item > a.nav-link{ text-transform:capitalize; }
